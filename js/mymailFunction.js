89f1a458800525db3a8ca79a6eda95eea32786bf
Beat Schmid <info@theschmidfamily.ch>
2021-11-20 21:24:07 +0100

    201121 21:35

--- js/mymailFunction.js
+++ js/mymailFunction.js
@@ -2,9 +2,6 @@ const btn = document.getElementById('btn2')
 const inputs = document.querySelector('form')
 btn.addEventListener('click', () => {
   Email.send({
-    //Host: "smtp.mailtrap.io",
-    //Username: "b1d1f07194fb89",
-    //Password: "e627e01a9be20d",
     SecureToken: "8a407fea-78c2-4af5-859e-86a210b38221",
     To: "info@theschmidfamily.ch",
     From: inputs.elements["email"].value,
